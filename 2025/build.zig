36dadecc2122f547ba76cf3bf932143a33ef6a04
quangdn42 <quangdn42@gmail.com>
2025-12-12 18:39:32 -0500

    update build.zig script

--- 2025/build.zig
+++ 2025/build.zig
@@ -1,21 +1,33 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+const input = enum {
+    example,
+    full,
+
+    pub fn path(i: input, day: u8, buf: []u8) []const u8 {
+        var path_buf: [64]u8 = undefined;
+        const cwd_path = std.fs.cwd().realpath(".", &path_buf) catch @panic("failed to get cwd's real path");
+        return switch (i) {
+            .example => std.fmt.bufPrint(buf, "{s}/{d:0>2}/example.txt", .{ cwd_path, day }) catch @panic("OOM"),
+            .full => std.fmt.bufPrint(buf, "{s}/{d:0>2}/input.txt", .{ cwd_path, day }) catch @panic("OOM"),
+        };
+    }
+};
+
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const day_option = b.option(u8, "day", "Advent of Code day to compile") orelse 0;
-
-    var day_buf: [2]u8 = undefined;
-    const day_exe_name = std.fmt.bufPrint(&day_buf, "{d:0>2}", .{day_option}) catch @panic("OOM");
+    const day_option = b.option(u8, "day", "Advent of Code day to compile") orelse 1;
+    const input_option = b.option(input, "input", "Input file") orelse .example;
 
     var path_buf: [64]u8 = undefined;
-    const path_string = std.fmt.bufPrint(&path_buf, "{s}/main.cpp", .{day_exe_name}) catch @panic("OOM");
+    const source_path = std.fmt.bufPrint(&path_buf, "{d:0>2}/main.cpp", .{day_option}) catch @panic("OOM");
 
     var cwd = std.fs.cwd();
-    cwd.access(path_string, .{ .mode = .read_only }) catch |err| switch (err) {
+    cwd.access(source_path, .{ .mode = .read_only }) catch |err| switch (err) {
         error.FileNotFound => {
-            std.debug.print("FileNotFound: {s}\n", .{path_string});
+            std.debug.print("FileNotFound: {s}\n", .{source_path});
             std.process.exit(1);
         },
         else => |e| {
@@ -25,14 +37,14 @@ pub fn build(b: *std.Build) void {
     };
 
     const exe = b.addExecutable(.{
-        .name = day_exe_name,
+        .name = "aoc_2025",
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
             .link_libcpp = true,
         }),
     });
-    exe.root_module.addCSourceFile(.{ .file = b.path(path_string), .language = .cpp });
+    exe.root_module.addCSourceFile(.{ .file = b.path(source_path), .language = .cpp });
 
     b.installArtifact(exe);
 
@@ -42,6 +54,9 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     run_cmd.step.dependOn(b.getInstallStep());
+
+    var arg_buf: [64]u8 = undefined;
+    run_cmd.addArg(input_option.path(day_option, &arg_buf));
 
     if (b.args) |args| {
         run_cmd.addArgs(args);
