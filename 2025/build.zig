973755273824b5df07543b4934dc7a30eb704bf5
quangdn42 <quangdn42@gmail.com>
2025-12-26 23:38:16 -0500

    build.zig - optionally build with zig solution

--- 2025/build.zig
+++ 2025/build.zig
@@ -1,25 +1,12 @@
 const std = @import("std");
 
-const input = enum {
-    example,
-    full,
-
-    pub fn path(i: input, day: u8, buf: []u8) []const u8 {
-        var path_buf: [64]u8 = undefined;
-        const cwd_path = std.fs.cwd().realpath(".", &path_buf) catch @panic("failed to get cwd's real path");
-        return switch (i) {
-            .example => std.fmt.bufPrint(buf, "{s}/{d:0>2}/example.txt", .{ cwd_path, day }) catch @panic("OOM"),
-            .full => std.fmt.bufPrint(buf, "{s}/{d:0>2}/input.txt", .{ cwd_path, day }) catch @panic("OOM"),
-        };
-    }
-};
-
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     var day_option = b.option(u8, "day", "Advent of Code day to compile") orelse 0;
     const input_option = b.option(input, "input", "Input file") orelse .example;
+    const lang_option = b.option(lang, "lang", "Language the solution is in") orelse .cpp;
 
     var cwd = std.fs.cwd();
     if (day_option == 0) {
@@ -45,7 +32,11 @@ pub fn build(b: *std.Build) !void {
     }
 
     var path_buf: [64]u8 = undefined;
-    const source_path = std.fmt.bufPrint(&path_buf, "{d:0>2}/main.cpp", .{day_option}) catch @panic("OOM");
+    const source_path = std.fmt.bufPrint(
+        &path_buf,
+        "{d:0>2}/main.{s}",
+        .{ day_option, lang_option.file_ext() },
+    ) catch @panic("OOM");
 
     cwd.access(source_path, .{ .mode = .read_only }) catch |err| switch (err) {
         error.FileNotFound => {
@@ -58,15 +49,27 @@ pub fn build(b: *std.Build) !void {
         },
     };
 
-    const exe = b.addExecutable(.{
-        .name = "aoc_2025",
-        .root_module = b.createModule(.{
+    const exe_mod = blk: switch (lang_option) {
+        .cpp => {
+            var mod = b.createModule(.{
+                .target = target,
+                .optimize = optimize,
+                .link_libcpp = true,
+            });
+            mod.addCSourceFile(.{ .file = b.path(source_path), .language = .cpp });
+            break :blk mod;
+        },
+        .zig => b.createModule(.{
             .target = target,
             .optimize = optimize,
-            .link_libcpp = true,
+            .root_source_file = b.path(source_path),
         }),
+    };
+
+    const exe = b.addExecutable(.{
+        .name = "aoc_2025",
+        .root_module = exe_mod,
     });
-    exe.root_module.addCSourceFile(.{ .file = b.path(source_path), .language = .cpp });
 
     b.installArtifact(exe);
 
@@ -83,16 +86,30 @@ pub fn build(b: *std.Build) !void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // Just like flags, top level steps are also listed in the `--help` menu.
-    //
-    // The Zig build system is entirely implemented in userland, which means
-    // that it cannot hook into private compiler APIs. All compilation work
-    // orchestrated by the build system will result in other Zig compiler
-    // subcommands being invoked with the right flags defined. You can observe
-    // these invocations when one fails (or you pass a flag to increase
-    // verbosity) to validate assumptions and diagnose problems.
-    //
-    // Lastly, the Zig build system is relatively simple and self-contained,
-    // and reading its source code will allow you to master it.
 }
+
+const input = enum {
+    example,
+    full,
+
+    pub fn path(i: input, day: u8, buf: []u8) []const u8 {
+        var path_buf: [64]u8 = undefined;
+        const cwd_path = std.fs.cwd().realpath(".", &path_buf) catch @panic("failed to get cwd's real path");
+        return switch (i) {
+            .example => std.fmt.bufPrint(buf, "{s}/{d:0>2}/example.txt", .{ cwd_path, day }) catch @panic("OOM"),
+            .full => std.fmt.bufPrint(buf, "{s}/{d:0>2}/input.txt", .{ cwd_path, day }) catch @panic("OOM"),
+        };
+    }
+};
+
+const lang = enum {
+    cpp,
+    zig,
+
+    fn file_ext(l: lang) []const u8 {
+        return switch (l) {
+            .cpp => "cpp",
+            .zig => "zig",
+        };
+    }
+};
